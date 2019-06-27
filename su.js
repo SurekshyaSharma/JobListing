12617c3a4a8c23ff666ec6cc37ecb5c17cc91fc0
[Surekshya Sharma] <[xst-shar3415@stthomas.edu]>
2019-06-27 14:51:08 -0500

    Styling dropdown

--- su.js
+++ su.js
@@ -4,7 +4,8 @@ $(document).ready(function () {
     console.log("Here We Go");
 
     getUSTjoblisitng();
-   
+   getfilter();
+ 
 
 }); 
 
@@ -72,7 +73,8 @@ function getUSTjoblisitng() {
              console.log(location);
             // console.log(department);
             // console.log(positionType);
-
+         
+//appending in the button
             $.each(location, function (key, value){
                 //console.log(value);
                 $('#loc').append('<a id="myDropdown href="locat">'+value+'</a>')
@@ -84,6 +86,55 @@ function getUSTjoblisitng() {
                 $('#pt').append('<a id="myDropdown href="depert">'+value+'</a>')
             });
         }
+    
 
     })
+}
+
+
+function getfilter(){
+    var csvfile = '/jobsample.csv';
+    var jsonformat;
+    console.log("(ajax-call)Starting from filter function.........");
+//converting the csv file into json format
+    $.ajax({
+        type: "GET",
+        url: csvfile,
+        dataType: "text",
+
+        error: function(error){
+            alert("Error Occurred");
+            console.log("Failed ajax call from filter function" , error);
+        },
+        
+        success: function(data){
+            jsonformat = $.csv.toObjects(data);
+
+            var location=  new Array();     
+            var department=  new Array();  
+            var positionType=  new Array();  
+
+            var ref_location= loc;
+            var ref_department=dep;
+            var ref_jobType=pt;
+            
+            $.each(jsonformat, function (index, value){
+                if (!positionType.includes(value['Employment Type'])){
+                    positionType.push(value['Employment Type']);
+                }
+                if (!department.includes(value['Department/Unit'])){
+                    department.push(value['Department/Unit']);
+                }
+                if (!location.includes(value['Campus : Location'])){
+                    location.push(value['Campus : Location']);
+                }
+                
+ 
+            }); 
+            
+            fill_filter(positionTypes, ref_jobType);
+            fill_filter(department, ref_department);
+            fill_filter(location, ref_location);
+        }
+    });
 }
